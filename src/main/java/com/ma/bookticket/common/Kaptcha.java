b11c910a3fc4703dc02bfc59eb914089934a9285
勇 <15913967074@163.com>
2021-01-29 16:11:12 +0800

    验证码生成器和工具类

---
+++ src/main/java/com/ma/bookticket/common/Kaptcha.java
@@ -0,0 +1,62 @@
+package com.ma.bookticket.common;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * 验证码生成器
+ *
+ * @author yong
+ * @date 2021/1/18 16:32
+ */
+@Component
+public class Kaptcha {
+    @Bean
+    public DefaultKaptcha getDefaultKaptcha() {
+        com.google.code.kaptcha.impl.DefaultKaptcha defaultKaptcha = new com.google.code.kaptcha.impl.DefaultKaptcha();
+        Properties properties = new Properties();
+        // 图片边框
+        properties.setProperty("kaptcha.border", "no");
+        // 边框颜色
+        properties.setProperty("kaptcha.border.color", "black");
+        //边框厚度
+        properties.setProperty("kaptcha.border.thickness", "1");
+        // 图片宽
+        properties.setProperty("kaptcha.image.width", "120");
+        // 图片高
+        properties.setProperty("kaptcha.image.height", "40");
+        //图片实现类
+        properties.setProperty("kaptcha.producer.impl", "com.google.code.kaptcha.impl.DefaultKaptcha");
+        //文本实现类
+        properties.setProperty("kaptcha.textproducer.impl", "com.google.code.kaptcha.text.impl.DefaultTextCreator");
+        //文本集合，验证码值从此集合中获取
+        properties.setProperty("kaptcha.textproducer.char.string", "01234567890");
+        //验证码长度
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        //字体
+        properties.setProperty("kaptcha.textproducer.font.names", "宋体");
+        //字体颜色
+        properties.setProperty("kaptcha.textproducer.font.color", "black");
+        //文字间隔
+        properties.setProperty("kaptcha.textproducer.char.space", "5");
+        //干扰实现类
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.DefaultNoise");
+        //干扰颜色
+        properties.setProperty("kaptcha.noise.color", "blue");
+        //干扰图片样式
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
+        //背景实现类
+        properties.setProperty("kaptcha.background.impl", "com.google.code.kaptcha.impl.DefaultBackground");
+        //背景颜色渐变，结束颜色
+        properties.setProperty("kaptcha.background.clear.to", "white");
+        //文字渲染器
+        properties.setProperty("kaptcha.word.impl", "com.google.code.kaptcha.text.impl.DefaultWordRenderer");
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+}
---
+++ src/main/java/com/ma/bookticket/utils/KaptchaUtils.java
@@ -0,0 +1,50 @@
+package com.ma.bookticket.utils;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+
+/**
+ * 请求验证码工具类
+ * 生成带图片等信息的ServletOutputStream，并在HttpSession属性里添加相应的验证码文本信息
+ * @author yong
+ * @date 2021/1/18 16:27
+ */
+public class KaptchaUtils {
+    public static void validateCode(HttpServletRequest request, HttpServletResponse response, DefaultKaptcha captchaProducer, String validateSessionKey) throws Exception{
+        // Set to expire far in the past.
+        response.setDateHeader("Expires", 0);
+        // Set standard HTTP/1.1 no-cache headers.
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        // Set standard HTTP/1.0 no-cache header.
+        response.setHeader("Pragma", "no-cache");
+
+        // return a jpeg
+        response.setContentType("image/jpeg");
+
+        // create the text for the image
+        String capText = captchaProducer.createText();
+
+        // store the text in the session
+        request.getSession().setAttribute(validateSessionKey, capText);
+
+        // create the image with the text
+        BufferedImage bi = captchaProducer.createImage(capText);
+
+        ServletOutputStream out = response.getOutputStream();
+
+        // write the data out
+        ImageIO.write(bi, "jpg", out);
+        try {
+            out.flush();
+        } finally {
+            out.close();
+        }
+    }
+}
