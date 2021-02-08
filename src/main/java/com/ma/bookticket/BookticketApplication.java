2637d61b5cf05d2af4fa4f2a3341cd79589d72fd
勇 <15913967074@163.com>
2021-02-08 19:32:13 +0800

    开启事务支持

--- src/main/java/com/ma/bookticket/BookticketApplication.java
+++ src/main/java/com/ma/bookticket/BookticketApplication.java
@@ -3,10 +3,12 @@ package com.ma.bookticket;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @MapperScan("com.ma.bookticket.mapper")
+@EnableTransactionManagement(proxyTargetClass = true) //开启事务支持
 public class BookticketApplication {
 
     public static void main(String[] args) {
